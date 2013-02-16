7f31b646eaabd03051af8c7abc59d4a01350d6c3
jtakagicorp <jtakagi_corp@yahoo.co.jp>
2013-02-17 08:57:36 +0900

    reverted MirrorActivity

--- Mirror/src/my/hackday/Mirror/MirrorActivity.java
+++ Mirror/src/my/hackday/Mirror/MirrorActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.util.Log;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MirrorActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */	
@@ -38,8 +37,5 @@ public class MirrorActivity extends Activity implements OnClickListener {
     		default:
     			break;
     	}
-    	Pusher pusher = new Pusher();
-    	pusher.getRequest("post", "value");
-    	Log.i("debug","pusher");
     }
 }
