742ef8a7dfade9285381d4459db377adf7a9dab3
akigit <aquimiquan@yahoo.co.jp>
2013-02-16 12:28:01 +0900

    ボタンの追加

--- Mirror/src/my/hackday/Mirror/MirrorActivity.java
+++ Mirror/src/my/hackday/Mirror/MirrorActivity.java
@@ -1,13 +1,28 @@
 package my.hackday.Mirror;
 
 import android.app.Activity;
+import android.view.View.OnClickListener;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MirrorActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+public class MirrorActivity extends Activity implements OnClickListener {
+    /** Called when the activity is first created. */	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button changeColorButton = (Button) findViewById(R.id.change_color_button);
+        changeColorButton.setOnClickListener(this);
+    }
+    
+    public void onClick(View view) {
+    	switch (view.getId()) {
+    		case R.id.change_color_button:
+    			break;
+    		default:
+    			break;
+    	}
     }
 }
