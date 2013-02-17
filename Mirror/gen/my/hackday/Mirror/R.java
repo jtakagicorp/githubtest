953d1cf814a8579d51f6be04f7969b3da7b725f2
akigit <aquimiquan@yahoo.co.jp>
2013-02-17 13:44:40 +0900

    music add

--- Mirror/gen/my/hackday/Mirror/R.java
+++ Mirror/gen/my/hackday/Mirror/R.java
@@ -8,34 +8,41 @@
 package my.hackday.Mirror;
 
 public final class R {
+    public static final class anim {
+        public static final int in_from_left=0x7f040000;
+        public static final int in_from_right=0x7f040001;
+        public static final int out_to_left=0x7f040002;
+        public static final int out_to_right=0x7f040003;
+    }
     public static final class attr {
     }
     public static final class drawable {
-        public static final int button_hikari=0x7f020000;
-        public static final int button_oto=0x7f020001;
-        public static final int hikari_01=0x7f020002;
-        public static final int hikari_02=0x7f020003;
-        public static final int hikari_03=0x7f020004;
-        public static final int hikari_04=0x7f020005;
-        public static final int hikari_05=0x7f020006;
-        public static final int hikari_06=0x7f020007;
-        public static final int hikari_07=0x7f020008;
-        public static final int ic_launcher=0x7f020009;
-        public static final int partyhack=0x7f02000a;
-        public static final int sound_default=0x7f02000b;
-        public static final int sound_pressed=0x7f02000c;
+        public static final int ic_launcher=0x7f020000;
+        public static final int musicbackc=0x7f020001;
+        public static final int musicbackg=0x7f020002;
+        public static final int musicbackm=0x7f020003;
+        public static final int musicbacko=0x7f020004;
+        public static final int partyhack=0x7f020005;
+        public static final int sound_btn=0x7f020006;
+        public static final int sound_btn_default=0x7f020007;
+        public static final int sound_btn_pressed=0x7f020008;
     }
     public static final class id {
-        public static final int mirror_title=0x7f050000;
-        public static final int sound_button=0x7f050001;
+        public static final int ImageView01=0x7f060002;
+        public static final int ImageView02=0x7f060003;
+        public static final int ImageView03=0x7f060004;
+        public static final int ImageView04=0x7f060005;
+        public static final int ViewFlipper=0x7f060001;
+        public static final int mirror_layout=0x7f060000;
+        public static final int sound_button=0x7f060006;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
-        public static final int light_name=0x7f040002;
-        public static final int sound_name=0x7f040003;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
+        public static final int light_name=0x7f050002;
+        public static final int sound_name=0x7f050003;
     }
 }
--- Mirror/src/my/hackday/Mirror/MirrorActivity.java
+++ Mirror/src/my/hackday/Mirror/MirrorActivity.java
@@ -1,32 +1,126 @@
 package my.hackday.Mirror;
 
 import android.app.Activity;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageButton;
+import android.widget.ViewFlipper;
+
+import my.hackday.Mirror.Pusher;
 
 public class MirrorActivity extends Activity implements OnClickListener {
-    /** Called when the activity is first created. */	
+	private GestureDetector mGestureDetector;
+	private Pusher mPusher = null;
+	
+	private int mColorNum = 1;
+	private ImageButton mSoundButton = null;
+	
+	ViewFlipper viewFlipper;
+	Animation inFromRightAnimation;
+	Animation inFromLeftAnimation;
+	Animation outToRightAnimation;
+	Animation outToLeftAnimation;
+  
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
         
-        // ƒ{ƒ^ƒ“‚ÉƒNƒŠƒbƒNƒCƒxƒ“ƒg‚ğ‚Â‚¯‚é
-        ImageButton soundButton = (ImageButton) findViewById(R.id.sound_button);
-        soundButton.setOnClickListener(this);
+        mGestureDetector = new GestureDetector(this, simpleOnGestureListener);
+        mPusher = new Pusher();
+        
+        viewFlipper = (ViewFlipper) findViewById(R.id.ViewFlipper);
+        mSoundButton = (ImageButton) findViewById(R.id.sound_button);
+        mSoundButton.setOnClickListener(this);
+        
+        setAnimations();
     }
     
+	protected void setAnimations() {    
+		inFromRightAnimation = 
+	    AnimationUtils.loadAnimation(this, R.anim.in_from_right);
+		inFromLeftAnimation = 
+	    AnimationUtils.loadAnimation(this, R.anim.in_from_left);
+		outToRightAnimation = 
+		AnimationUtils.loadAnimation(this, R.anim.out_to_right);
+		outToLeftAnimation = 
+		AnimationUtils.loadAnimation(this, R.anim.out_to_left);
+	}
+	
+	protected void changeScale() {
+    	AnimationSet set = new AnimationSet(true);
+
+		Animation scale = new ScaleAnimation(1, 1, 1, 1.1f, 100, 0);
+       	Animation trans = new TranslateAnimation(0, 0, 0, -20);
+    	
+    	set.addAnimation(scale);
+    	set.addAnimation(trans);
+    	set.setDuration(200);
+    	
+    	mSoundButton.startAnimation(set);
+	}
+	
     public void onClick(View view) {
     	switch (view.getId()) {
     		case R.id.sound_button:
-    			// ‰¹Šy‚ğ–Â‚ç‚·
+    			// éŸ³æ¥½ã‚’é³´ã‚‰ã™
+    			mPusher.getRequest("tap", "1");
+    			changeScale();
     			break;
     		default:
     			break;
     	}
     }
+    
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+    	mGestureDetector.onTouchEvent(event);      
+        return super.onTouchEvent(event); 
+    }
+    
+    private SimpleOnGestureListener simpleOnGestureListener = new SimpleOnGestureListener() { 
+        @Override 
+        public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
+        	// ãƒ•ãƒªãƒƒã‚¯ã‚¤ãƒ™ãƒ³ãƒˆ
+            float dx = Math.abs(velocityX);
+            float dy = Math.abs(velocityY);
+            
+            if (dx > dy && dx > 150) {
+                if (event1.getX() < event2.getX()) {
+        			viewFlipper.setInAnimation(inFromLeftAnimation);
+        		    viewFlipper.setOutAnimation(outToRightAnimation);
+        		    viewFlipper.showPrevious();
+        		    
+        		    mPusher.getRequest("swipe", String.valueOf(mColorNum));
+        		    mColorNum --;
+                } else {
+        			viewFlipper.setInAnimation(inFromRightAnimation);
+        		    viewFlipper.setOutAnimation(outToLeftAnimation);
+                    viewFlipper.showNext();
+                    
+         		    mPusher.getRequest("swipe", String.valueOf(mColorNum));
+         		    mColorNum ++;
+                }
+            }
+		    
+            return super.onFling(event1, event2, velocityX, velocityY); 
+        } 
+
+        @Override 
+        public boolean onSingleTapConfirmed(MotionEvent event) { 
+        	// ã‚¿ãƒƒãƒ—ã‚¤ãƒ™ãƒ³ãƒˆ
+            return super.onSingleTapConfirmed(event); 
+        }
+    };
 }
